fec664c29da534ff7c9f35e1eede61deb6a445b7
ZealKamau <zeekthedev@gmail.com>
2023-02-17 03:34:22 +0300

    program that prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-include <stdio.h>
-
+#!/bin/bash
+#include <stdio.h>
 /**
- ** main - Print a string
- ** Description: use printf
- ** Return: 0
- **/
-
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
