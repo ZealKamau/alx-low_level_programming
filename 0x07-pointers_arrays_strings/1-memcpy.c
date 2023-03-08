6b98c530b677b0e21c5463aa21362a1a9c9dda85
ZealKamau <zeekthedev@gmail.com>
2023-03-08 06:00:14 +0300

    houih

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,33 +7,33 @@
  *    * @size: the size of the memory to print
  *     *
  *      * Return: Nothing.
- *       */
+*/
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	    unsigned int i;
+unsigned int i;
 
-	        i = 0;
-		    while (i < size)
-			        {
-					        if (i % 10)
-							        {
-									            printf(" ");
-										            }
-						        if (!(i % 10) && i)
-								        {
-										            printf("\n");
-											            }
-							        printf("0x%02x", buffer[i]);
-								        i++;
-									    }
-		        printf("\n");
+i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
 }
 
 /**
  *  * main - check the code for ALX School students.
  *   *
  *    * Return: Always 0.
- *     */
+*/
 int main(void)
 {
 	    char buffer[98] = {0};
